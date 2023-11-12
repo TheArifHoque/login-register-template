96b97a5890c843bfd3627e0070de5fcd69cd7553
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2023-11-12 23:23:17 +0600

    initial commit

---
+++ src/main/java/com/arifhoque/main/LoginRegisterTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.arifhoque.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegisterTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginRegisterTemplateApplication.class, args);
+    }
+
+}
